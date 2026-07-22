90fd63add2a9d0968ce58f17f4e22db3396a2084
Sudipta Pal <purnob2003@gmail.com>
2026-07-22 22:17:41 +0600

    Added upto Attendance part by Sudipta

---
+++ client/src/pages/Attendance.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { dummyAttendanceData } from "../assets/assets";
+import Loading from "../components/Loading"; 
+import CheckInButton from "../components/attendence/CheckInButton";
+import AttendanceStats from "../components/attendence/AttendanceStats";
+import AttendanceHistory from "../components/attendence/AttendanceHistory"; 
+
+const Attendance = () => {
+
+    const [history, setHistory] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [isDeleted, setIsDeleted] = useState(false);
+
+    const fetchData = useCallback(async () => {
+        setHistory(dummyAttendanceData)
+        setTimeout(() => {
+            setLoading(false)
+        }, 1000)
+    }, [])
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
+    if(loading) return <Loading />;
+
+    const today = new Date()
+    today.setHours(0, 0, 0, 0); 
+    const todayRecord = history.find((r) => new Date(r.date).toDateString() === today.toDateString());
+
+    return(
+        <div className="animate-fade-in">
+            <div className="page-header">
+                <h1 className="page-title">Attendance</h1>
+                <p className="page-subtitle">Track your work hours and daily check-ins</p>
+            </div>
+
+            {isDeleted ? (
+                <div className="mb-8 p-6 bg-rose-50 border border-rose-200 rounded-2xl text-center">
+                    <p className="text-rose-600">You can no longer clock in or out because you employee records have been marked as deleted.</p>
+                </div>
+            ) : (
+                <div className="mb-6">
+                    <CheckInButton todayRecord={todayRecord} onAction={fetchData} />
+                </div>
+            )}
+
+            <AttendanceStats history={history} />
+            <AttendanceHistory history={history} />
+        </div>
+    )
+}
+
+export default Attendance 
