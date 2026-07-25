b00f60792a5054dad32d3bc4ef3a2fb70868a282
Sudipta Pal <purnob2003@gmail.com>
2026-07-25 20:27:45 +0600

    Added Payslip feature by Sudipta

--- client/src/pages/Payslips.jsx
+++ client/src/pages/Payslips.jsx
@@ -1,7 +1,44 @@
+import { useCallback, useEffect, useState } from "react"
+import { dummyEmployeeData, dummyPayslipData } from "../assets/assets"
+import PayslipList from "../components/payslip/PayslipList"
+import Loading from "../components/Loading"
+import GeneratePayslipForm from "../components/payslip/GeneratePayslipForm"
 
 const Payslips = () => {
+
+    const [payslips, setPayslips] = useState([])
+    const [employees, setEmployees] = useState([])
+    const [loading, setloading] = useState(true);
+    const isAdmin = true;
+
+    const fetchPayslips = useCallback(async ()=> {
+        setPayslips(dummyPayslipData)
+        setTimeout(() => {
+            setloading(false);
+        }, 1000);
+    }, [])
+
+    useEffect(() => {
+        fetchPayslips()
+    }, [fetchPayslips])
+
+    useEffect(()=>{
+        if(isAdmin) setEmployees(dummyEmployeeData)
+    }, [isAdmin])
+
+    if(loading) return <Loading />
+
     return(
-        <div> Payslips </div>
+        <div className="animate-fade-in">
+            <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb-8">
+                <div>
+                    <h1 className="page-title">Payslips</h1>
+                    <p className="page-subtitle">{isAdmin ? "Generate and mange employee payslips" : "Your payslip history"}</p>
+                </div>
+                {isAdmin && <GeneratePayslipForm employees={employees} onSuccess={fetchPayslips} />}
+            </div>
+            <PayslipList payslips={payslips} isAdmin={isAdmin} />
+        </div>
     )
 }
 
