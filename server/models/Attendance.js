9460c77c9a2624464067b6e75479c047ffb35656
Tahmid Alvee <151709545+tahmidalvee6@users.noreply.github.com>
2026-07-26 19:52:16 +0600

    Add files via upload

---
+++ server/models/Attendance.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const attendanceSchema = new mongoose.Schema({
+    employeeId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "Employee",
+        required: true     
+    },
+    date: {
+        type: Date,
+        required: true  
+    },
+    checkIn : {
+        type: Date,
+        default: null
+    },
+    checkOut : {   
+        type: Date, 
+        default: null
+    },
+    status: {
+        type: String,
+        enum: ['PRESENT', 'LATE', 'ABSENT', 'LEAVE'],
+        default: 'ABSENT'
+    },
+    workingHours: {
+        type: Number,
+        default: 0
+    },
+    dayType: {
+        type: String,
+        enum: ['FULL_DAY',"Three Quarter Day", 'HALF_DAY', "Short Day", null ],
+        default: null
+    }
+
+
+
+
+    
+},  { timestamps: true })
+
+attendanceSchema.index({ employeeId: 1, date: 1 }, { unique: true });
+
+const Attendance = mongoose.model("Attendance", attendanceSchema);
+
+export default Attendance;
