3643f061315b1ef84a966b6cdf44bc97bd207387
Tahmid Alvee <151709545+tahmidalvee6@users.noreply.github.com>
2026-07-26 19:54:50 +0600

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,36 @@
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import multer from "multer";
+import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import employeeRoutes from "./routes/employeeRoutes.js";
+import attendanceRouter from "./routes/attendanceRoutes.js";
+import profileRouter from "./routes/profileRoutes.js";
+import leaveRouter from "./routes/leaveRoutes.js";
+
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(multer().none())
+
+
+// Routes
+app.get("/", (req, res) =>res.send("Server is running..."));
+app.use("/api/auth", authRouter);
+app.use("/api/employees", employeeRoutes);
+app.use("/api/profile", profileRouter);
+app.use("/api/attendance", attendanceRouter);
+app.use("/api/leave", leaveRouter);
+
+await connectDB();
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
