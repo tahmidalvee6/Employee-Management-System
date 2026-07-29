5c6f7e820876073280b6899c28f35b266f570120
Tahmid Alvee <purnob2003@gmail.com>
2026-07-29 20:01:29 +0600

    Update payslipsRoutes.js

--- server/routes/payslipsRoutes.js
+++ server/routes/payslipsRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { protect, protectAdmin } from "../middleware/auth.js";
-import { createPayslip, getPayslipById, getPayslips } from "../controllers/payslipController.js";
+import { createPayslip, getPayslipById, getPayslip } from "../controller/payslipController.js";
 
 const payslipRouter = Router();
 
 payslipRouter.post("/", protect, protectAdmin, createPayslip)
-payslipRouter.get("/", protect, getPayslips)
+payslipRouter.get("/", protect, getPayslip)
 payslipRouter.get("/:id", protect, getPayslipById)
 
 export default payslipRouter
