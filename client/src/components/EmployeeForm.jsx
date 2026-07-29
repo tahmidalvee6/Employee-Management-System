6e86f51cbbb9e5432d05504027313233e5841720
Sudipta Pal <purnob2003@gmail.com>
2026-07-30 01:02:44 +0600

    completed remaining part

--- client/src/components/EmployeeForm.jsx
+++ client/src/components/EmployeeForm.jsx
@@ -2,14 +2,35 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { DEPARTMENTS } from "../assets/assets.jsx"
 import { Loader2Icon } from "lucide-react"
+import api from '../api/axios'
+import toast from 'react-hot-toast';
 
 const EmployeeForm = ({ initialData, onSuccess, onCancel }) => {
     const navigate = useNavigate()
     const [loading, setLoading] = useState(false)
     const isEditMode = !!initialData;
+
     const handleSubmit = async (e) => {
         e.preventDefault()
+        setLoading(true)
+        const formData = new FormData(e.currentTarget);
+        if(isEditMode) {
+            const pwd = formData.get("password")
+            if(!pwd) formData.delete("password")
+        }
+
+        try {
+            const url = isEditMode ? `/employees/${initialData.id}` : "/employees";
+            const method = isEditMode ? "put" : "post";
+            await api[method](url, formData)
+            onSuccess ? onSuccess() : navigate("/employees")
+        } catch(error) {
+            toast.error(err.response?.data?.error || err.message);
+        } finally {
+            setLoading(false);
+        }
     }
+
     return (
         <form onSubmit={handleSubmit} className="space-y-6 max-w-3xl animate-fade-in">
             {/* Personal Information */}
