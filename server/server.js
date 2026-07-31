fbfb66cd06fbf925382e38b05b477a5c7e7e219b
Tahmid Alvee <purnob2003@gmail.com>
2026-07-31 09:45:37 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import dns from "node:dns";
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
@@ -14,20 +13,19 @@ import profileRouter from "./routes/profileRoutes.js";
 import leaveRouter from "./routes/leaveRoutes.js";
 import payslipRouter from "./routes/payslipsRoutes.js";
 import dashboardRouter from "./routes/dashboardRoutes.js";
-
-
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://employee-management-system-ems-seven.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 app.use(multer().none())
-
 
 // Routes
 app.get("/", (req, res) =>res.send("Server is running..."));
@@ -38,8 +36,8 @@ app.use("/api/attendance", attendanceRouter);
 app.use("/api/leave", leaveRouter);
 app.use("/api/payslips", payslipRouter);
 app.use("/api/dashboard", dashboardRouter);
-
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 await connectDB();
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
