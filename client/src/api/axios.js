e761b305c082741021a0495384263c4e3edb8e16
Tahmid Alvee <purnob2003@gmail.com>
2026-07-31 09:42:49 +0600

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: (import.meta.env.VITE_BASE_URL || "http://localhost:4000") + "/api",
+    baseURL: (import.meta.env.VITE_BASE_URL || "https://employee-management-system-0gu1.onrender.com") + "/api",
     });
 
     // attach the token to every request if it exists
